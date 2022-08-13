401a518d68a4ae77d6bb04be17e3a8e1148c8947
AlmaF <alma@tura.io>
2022-08-13 12:28:51 -0700

    add regex clue for searching Alice text

---
+++ bacon_binary.py
@@ -0,0 +1,17 @@
+bacon_binary = "Jbe Dksin dsdura myjhgb mus ebuutgn rs tjo Ufibkrytjkd hodvugap Cyl Ftkdwiu Lqwop, \
+    gxi iu kbmo ebuxivot qivr yhvgxjcni dxy sesuhtkpyw mgdxid. Kd'i nhg zhydgmumsqb ez mqnuln CCSCI dsdura mexe, \
+    cxt un gkhfy ghqgpno ez svowunqqhupji, jbe rbqwtkmu if eydwecvyhg pyj duud q geucqae dej nhg pqwt vrqn tjo lceyoh \
+    cs nyeeipq qn a jstxep wumscqu ut cvb. Cn Dksin'u myjhgb, uucj vuntgb ez tjo qfpjkryt eyhleuzehdu de u fkfu-whcbqwtgb \
+    futvohh, wjohy ecmx whcbqwtgb sun vkay opo ez tyy viroc (mbav Lqwop mqflgn q-zotwi unf l-viroc, qhd yo miunn diw ekbf \
+    a dsj). Zot sdmtcxsy, tjo bytvoh 'V' mczi no 'ckqub', cxt nhg vuntgb 'S' garc ji 'ackru'. Tjo wynkei if vrym ckzxyr \
+    kc jbav dxy a-hyhgs cxt v-fqbcm ccx qhy vge xiudyhcv mqneiyhceu, vyee ibus ot lbucm zyrenc yh ap scugg, k hiw qp \
+    jolkzi unf zunupsqm ip k vfoyohvef, yh nhg mefot yv nhtoqxs kx q mhkbj; u ccckul xsuqet geolfx'j hovssy tjkj nhgi \
+    myrg veikkxw ut cx uhctifnef wumscqu. Nhg myjhgb sun cvii bg eiyd kx din-xsioan wuxic, vyee vru ponecy oh xeneu sd u \
+    sqxw. Nhgbu'm exod u dqmkgepdux ezkcjlg yv u sesuhtkcj ulvohcni dxy tgwfyrcdkle qp q ziur xubkdqn sq dxut vru ziuri' \
+    itqvynhu—leheu dxut cnt u lciul ecmx secb bckg dxy rkxwm oh k jleg—gule gxslyrdux tq buud, ossloumejiekbfy, 'js cim'. \
+    \
+    Hbeg tjo ccd-1800'u de nhg 1940'c jbeto mus c pqx tq megb vru jlcii if Urqeeuzuurg, eicni Lqwop'c scpjoh no nyee fqb \
+    iyctoj geucqaeu yh yvknuhcg dxut Ush Zrcxscs Dksin yki nhg buul cejbot yv Mhcuumpgkhy's yyhe. Au kd cnvohysvsda \
+    hkcjirkmqf austy, opo ez tjo virgwemt cnliccdum oh dxcs eydmpkbqwy vruira, Nufic Lqwop (xe lenkjcop), rqjpgxux tq lu u \
+    ftsuhd qp Iumwob Gotcu, cnxodnot yv Gotcu wofo."
+
---
+++ regex_clue.py
@@ -0,0 +1,6 @@
+regex_clue = "Ah `razkw.tq`, ue tomjtl zidd bzi tifz 'Rtagw ce Lfvvijfrcu', \
+    bzijy'j toiuxds fcv egvv gritpary nyxj lwwulzekqgr: \
+    An jirzlw ockw r tgawltpjm dilnvg zv llw uceyitil vvinmwr 'd' ues 'h'. \
+    Bzi kytdel dilnvg za FSL 'l', 'f', 'p', 'v', wj 't'. \
+    Szkti bzel nytim svw zzkv wj qglv afewvuujt cmlxwlj."
+    
