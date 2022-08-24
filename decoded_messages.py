b77376de2547bcca82f786c31adb265a3eff7740
parvister <parham@tura.io>
2022-08-24 13:28:52 -0700

    slight formatting edits on README. Added treasure hunt notebook

--- decoded_messages.py
+++ decoded_messages.py
@@ -1,18 +1,18 @@
 from regex_clue import regex_clue
 from site_address import site_address
 from bacon_binary import bacon_binary
-from vigenere import vigenere
+from vigenere import decrypt
 
 # first decoded message
 # uncomment, pass the key you got solving today.py as the third arg:
 
-# print(vigenere("d", regex_clue, ))
+# print(decrypt(regex_clue, ))
 
 # second decoded message
 # uncomment, pass the key you got solving regex_clue.py as the third arg:
 
-# print(vigenere("d", site_address, ))
+# print(decrypt(site_address, ))
 
 # third decoded message, today's bonus lesson.
 # uncomment, pass the key you got solving the Pandas mystery as the third arg:
-# print(vigenere("d", bacon_binary, ))
+# print(decrypt(bacon_binary, ))
