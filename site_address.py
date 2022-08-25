b9ba95ced769c3055d4813b77f5c739d325fe344
AlmaF <alma@tura.io>
2022-08-25 16:50:43 -0700

    move step 4 Pandas clue to different site

--- site_address.py
+++ site_address.py
@@ -1,2 +1,2 @@
-site_address = "wbvim://xmlwcd.vid/edbi-hkueowcavxce/azdlwghzx"
+site_address = "wbvim://usuh.oqhaci.udu/rkyjifiivbie/h/w/2PACX-1kQkzNNwFoEYPCPvDH-ww22-jS2S_MdR1-YiD2ZSqvmpRW7aTmUChhLX-RbYm0g6xxZqaVG2ECrPJZH-/ios?wlpzv=yucww&awqi=zrpkt&lgeupqk=3000"
 
