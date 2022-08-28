30a050b7a4b065bbe175793528e28d68d4989f91
AlmaF <alma@tura.io>
2022-08-28 15:59:19 -0700

    use triple quotes instead of newlines, easier to read

--- bacon_binary.py
+++ bacon_binary.py
@@ -1,17 +1,33 @@
-bacon_binary = "Txy Baeyd vipkhs ckzxyr yki wrgkjyd di jbe Enspubgdxun pyrfeokd Scr Ftkdwiu Bksin, \
-    yre cs cvii ctotctgn mctj sdpepdyhg vru mckodnihss gevrex. It'u dxy ptotycgciir qp cidgbd ASCII vipkhs cqnu, \
-    unf kd yatvo yxcwffe qp ineikdigtkfby, vru jrcmjccg yv wopmuulkxw hov tkmt c wumscqu vuv dxy fcmj nhcd jbe xsuqet \
-    si foquyhg cd q bifnuh mgciugg kj uln. Ix Bqwop'c scpjoh, yaer bytvoh if vru ulrrqvev melrgcfinfc ji a hsly-cjkhucvoh \
-    javduln, yrule gksb cjkhucvoh wap dqee qxu if vge zotwi (qhcd Bqwop mqflgn q-zotwi unf l-viroc, qhd yo miunn diw ekbf \
-    a dsj). Fir kxinapmu, nhg vuntgb 'B' cupu de 'uackr', unf dxy lgdjyr 'C' okfm tq 'kqubc'. Tru aepskm oh dxcs esfbet \
-    si nhcd jbe c-pelmu kdx b-hyhgs ekd una dmi dkcjcned sutgqeligc, bckg qhyy qb rfaeu fcxgvi cn cx ygaio, q loy yv \
-    nunsfm apn fytwxyus kx q zlqgulbgn, el tjo silqb ez tjbuudu sd u sjshn; a ekioan fyywgb miunnd'n nqdywe vrqn tjoo \
-    qeto biomsda av kd ynebojtgn cysukwy. Thg myjhgb sun cvii bg eiyd kx din-xsioan wuxic, vyee vru ponecy oh xeneu sd u \
-    sqxw. Tbeto'i yvgx q xoeecynvot yxcwffe qp q mckodniud qftgbyhg vru neozulavehy oh k vcsj rqvivkj mo vrqn tjo vcsjc' \
-    enonsjbs (dydys vrqn afn q faaoh yaer oyat vyee vru lipqi if c dhye) yohy epmhspvot no toqx, mkmhiseyfcccvbs, 'hk weg'. \
-    \
-    Frqw jbe ost-1800'm tq dxy 1940's vrule yki u fcn ji cqwr nhg zbuyu yv Sbamoijecbu, oskxw Bucqx'i wirrul tq veik hyh \
-    meebun mgciuggc el exstyneo jbav Ssh Flapmym Baeyd qau dxy rgkb uuvrel oh Srqeeuzuurg'c mirm. Ac qh ipduleudyhg \
-    jsinotssul ccyxe, qxu if vru zotocisv ktpoekjys qp jbiu mehsrshuca dxyoti, Dufic Bksin (py hylcdyin), jkfjepot no do q \
-    zrkodx oh Skcoen Myhme, kxlynvyh if Mqbiy cqnu."
+# bacon_binary = "Txy Baeyd vipkhs ckzxyr yki wrgkjyd di jbe Enspubgdxun pyrfeokd Scr Ftkdwiu Bksin, \
+#     yre cs cvii ctotctgn mctj sdpepdyhg vru mckodnihss gevrex. It'u dxy ptotycgciir qp cidgbd ASCII vipkhs cqnu, \
+#     unf kd yatvo yxcwffe qp ineikdigtkfby, vru jrcmjccg yv wopmuulkxw hov tkmt c wumscqu vuv dxy fcmj nhcd jbe xsuqet \
+#     si foquyhg cd q bifnuh mgciugg kj uln. Ix Bqwop'c scpjoh, yaer bytvoh if vru ulrrqvev melrgcfinfc ji a hsly-cjkhucvoh \
+#     javduln, yrule gksb cjkhucvoh wap dqee qxu if vge zotwi (qhcd Bqwop mqflgn q-zotwi unf l-viroc, qhd yo miunn diw ekbf \
+#     a dsj). Fir kxinapmu, nhg vuntgb 'B' cupu de 'uackr', unf dxy lgdjyr 'C' okfm tq 'kqubc'. Tru aepskm oh dxcs esfbet \
+#     si nhcd jbe c-pelmu kdx b-hyhgs ekd una dmi dkcjcned sutgqeligc, bckg qhyy qb rfaeu fcxgvi cn cx ygaio, q loy yv \
+#     nunsfm apn fytwxyus kx q zlqgulbgn, el tjo silqb ez tjbuudu sd u sjshn; a ekioan fyywgb miunnd'n nqdywe vrqn tjoo \
+#     qeto biomsda av kd ynebojtgn cysukwy. Thg myjhgb sun cvii bg eiyd kx din-xsioan wuxic, vyee vru ponecy oh xeneu sd u \
+#     sqxw. Tbeto'i yvgx q xoeecynvot yxcwffe qp q mckodniud qftgbyhg vru neozulavehy oh k vcsj rqvivkj mo vrqn tjo vcsjc' \
+#     enonsjbs (dydys vrqn afn q faaoh yaer oyat vyee vru lipqi if c dhye) yohy epmhspvot no toqx, mkmhiseyfcccvbs, 'hk weg'. \
+#     \
+#     Frqw jbe ost-1800'm tq dxy 1940's vrule yki u fcn ji cqwr nhg zbuyu yv Sbamoijecbu, oskxw Bucqx'i wirrul tq veik hyh \
+#     meebun mgciuggc el exstyneo jbav Ssh Flapmym Baeyd qau dxy rgkb uuvrel oh Srqeeuzuurg'c mirm. Ac qh ipduleudyhg \
+#     jsinotssul ccyxe, qxu if vru zotocisv ktpoekjys qp jbiu mehsrshuca dxyoti, Dufic Bksin (py hylcdyin), jkfjepot no do q \
+#     zrkodx oh Skcoen Myhme, kxlynvyh if Mqbiy cqnu."
 
+bacon_binary = """Txy Baeyd vipkhs ckzxyr yki wrgkjyd di jbe Enspubgdxun pyrfeokd Scr Ftkdwiu Bksin, 
+    yre cs cvii ctotctgn mctj sdpepdyhg vru mckodnihss gevrex. It'u dxy ptotycgciir qp cidgbd ASCII vipkhs cqnu, 
+    unf kd yatvo yxcwffe qp ineikdigtkfby, vru jrcmjccg yv wopmuulkxw hov tkmt c wumscqu vuv dxy fcmj nhcd jbe xsuqet 
+    si foquyhg cd q bifnuh mgciugg kj uln. Ix Bqwop'c scpjoh, yaer bytvoh if vru ulrrqvev melrgcfinfc ji a hsly-cjkhucvoh 
+    javduln, yrule gksb cjkhucvoh wap dqee qxu if vge zotwi (qhcd Bqwop mqflgn q-zotwi unf l-viroc, qhd yo miunn diw ekbf 
+    a dsj). Fir kxinapmu, nhg vuntgb 'B' cupu de 'uackr', unf dxy lgdjyr 'C' okfm tq 'kqubc'. Tru aepskm oh dxcs esfbet 
+    si nhcd jbe c-pelmu kdx b-hyhgs ekd una dmi dkcjcned sutgqeligc, bckg qhyy qb rfaeu fcxgvi cn cx ygaio, q loy yv 
+    nunsfm apn fytwxyus kx q zlqgulbgn, el tjo silqb ez tjbuudu sd u sjshn; a ekioan fyywgb miunnd'n nqdywe vrqn tjoo 
+    qeto biomsda av kd ynebojtgn cysukwy. Thg myjhgb sun cvii bg eiyd kx din-xsioan wuxic, vyee vru ponecy oh xeneu sd u 
+    sqxw. Tbeto'i yvgx q xoeecynvot yxcwffe qp q mckodniud qftgbyhg vru neozulavehy oh k vcsj rqvivkj mo vrqn tjo vcsjc' 
+    enonsjbs (dydys vrqn afn q faaoh yaer oyat vyee vru lipqi if c dhye) yohy epmhspvot no toqx, mkmhiseyfcccvbs, 'hk weg'. 
+    \
+    Frqw jbe ost-1800'm tq dxy 1940's vrule yki u fcn ji cqwr nhg zbuyu yv Sbamoijecbu, oskxw Bucqx'i wirrul tq veik hyh 
+    meebun mgciuggc el exstyneo jbav Ssh Flapmym Baeyd qau dxy rgkb uuvrel oh Srqeeuzuurg'c mirm. Ac qh ipduleudyhg 
+    jsinotssul ccyxe, qxu if vru zotocisv ktpoekjys qp jbiu mehsrshuca dxyoti, Dufic Bksin (py hylcdyin), jkfjepot no do q 
+    zrkodx oh Skcoen Myhme, kxlynvyh if Mqbiy cqnu."""
--- regex_clue.py
+++ regex_clue.py
@@ -1,8 +1,7 @@
-regex_clue = "If `ucxtm.hc`, sh vmvzhx xlfb kpw fgib 'Aazkw mf Wiesvzdefx', kwvzw'w wrrrktq sfy ndil eelwyxeo llam utjkjmhnzde:\
-    Ib kxslkh nqll s fflvzueky ctkbwv ah kwv idtzustk jwxoyvc 'c' ifh 'i'.\
-    Tbv hvkgrv fvikmj mk NOT 'l', 'f', 'p', 'v', wj 't'.\
-    Axnvg kpsx lbvgv iji xcmt fz esjy cdnmjgsmv avblijm.\
-    Tyt nwjh whuh nqll lbv avblij 'm'.\
-    Ie iym xmdy `uttwviv_gvhjiyik.jp`, jjm `kmly_rsuzwwk` uj iym xmjmk piomqwhk if `lwgjsgi()`\
-    rvv xzy ndil qsm avi wzgq `sfzrv.xq` ek nyt jmusfx rgxceifn. Tyt umuvqjktu\
-    uwwkuxt nqdp lyca pwm azyit kw ys fyoi."
+regex_clue = """If `ucxtm.hc`, sh vmvzhx xlfb kpw fgib 'Aazkw mf Wiesvzdefx', kwvzw'w wrrrktq sfy ndil eelwyxeo llam utjkjmhnzde:
+    Ib kxslkh nqll s fflvzueky ctkbwv ah kwv idtzustk jwxoyvc 'c' ifh 'i'.
+    Tbv hvkgrv fvikmj mk NOT 'l', 'f', 'p', 'v', wj 't'.
+    Axnvg kpsx lbvgv iji xcmt fz esjy cdnmjgsmv avblijm.
+    Tyt nwjh whuh nqll lbv avblij 'm'.
+    Ie iym xmdy `uttwviv_gvhjiyik.jp`, jjm `kmly_rsuzwwk` uj iym xmjmk piomqwhk if `lwgjsgi()`
+    rvv xzy ndil qsm avi wzgq `sfzrv.xq` ek nyt jmusfx rgxceifn. Tyt umuvqjktu uwwkuxt nqdp lyca pwm azyit kw ys fyoi."""
